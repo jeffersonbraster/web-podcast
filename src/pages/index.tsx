3d81ba34819d0ca8e29aa16e94b15d7cc11af37f
Jefferson <jeffersonbraster@gmail.com>
2021-04-23 08:58:25 -0300

    criando e utilizando o hook do player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import { api } from "../services/api";
 import { convertDurationToTypeString } from "../utils/convertDurationToTypeString";
 
 import styles from "./home.module.scss";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 type Episode = {
   id: string;
@@ -26,6 +28,7 @@ type HomeProps = {
 };
 
 export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
   return (
     <div className={styles.homePage}>
       <section className={styles.lastestEpisodes}>
@@ -52,7 +55,7 @@ export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar agora" />
                 </button>
               </li>
